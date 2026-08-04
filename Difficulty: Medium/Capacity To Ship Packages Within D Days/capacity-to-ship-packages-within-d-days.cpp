e1b1e3fba5594aa25a73881c49afe5b79d51b9aa
Shourya Sharma <sshouryas538@gmail.com>
2026-08-04 16:21:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.cpp
+++ Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.cpp
@@ -5,32 +5,35 @@ class Solution {
         int low = 0;
         int high = 0;
         for(int i=0;i<arr.size();i++){
-            low = max(low,arr[i]);
+            if(arr[i]>low) low = arr[i];
             high += arr[i];
         }
-        int ans = high;
+        int ans = 0;
         while(low<=high){
             int mid = low + (high - low)/2;
-            if(miniD(arr,D,mid)){
-                ans = mid;
+            if(capacity(arr,D,mid)){
                 high = mid -1;
+                ans = mid;
             }
-            else low = mid +1;
+            else{
+                low = mid +1;
+            }
         }
         return ans;
     }
-    bool miniD(vector<int>arr,int D,int mid){
-        int days = 1;
+    bool capacity(vector<int>arr,int D,int mid){
         int total = 0;
+        int Day = 1;
         for(int i=0;i<arr.size();i++){
-           if((total + arr[i] )> mid){
-               days++;
-               total = arr[i];
-           }
-           else {
-               total += arr[i];
-           }
+            if(arr[i]>mid) return false;
+            if(total + arr[i] > mid){
+                Day++;
+                total = arr[i];
+            }
+            else{
+                total += arr[i];
+            }
         }
-        return days<=D;
+        return Day<=D;
     }
 };
