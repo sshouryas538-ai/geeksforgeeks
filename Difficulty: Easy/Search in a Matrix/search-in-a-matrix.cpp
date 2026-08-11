c206008f4ceb632513d8101de919a9e4dfa327ae
Shourya Sharma <sshouryas538@gmail.com>
2026-08-11 20:05:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Matrix/search-in-a-matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[i].size();j++){
+                if(mat[i][j] == x) return true;
+            }
+        }
+        return false;
+    }
+};
