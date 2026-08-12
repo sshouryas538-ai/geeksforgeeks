5e3dbd7f742405550498df2eb63113235b685503
Shourya Sharma <sshouryas538@gmail.com>
2026-08-12 21:44:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse Digits/reverse-digits.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    int reverseDigits(int n){
+        int rev =0;
+        
+        while(n>0){
+            rev = rev*10 +(n%10);
+            n/=10;
+        }
+        return rev;
+    }
+};
