28d2c4060bcec2e3aa47a2e3383d1d8711a117c0
Shourya Sharma <sshouryas538@gmail.com>
2026-09-10 21:29:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Arrange Anagrams Together/arrange-anagrams-together.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<vector<string>> anagrams(vector<string>& arr) {
+        // code here
+        unordered_map<string,vector<string>>mpp;
+        for(auto it:arr){
+            string key = it;
+            sort(key.begin(),key.end());
+            mpp[key].push_back(it);
+        }
+        vector<vector<string>>ans;
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
