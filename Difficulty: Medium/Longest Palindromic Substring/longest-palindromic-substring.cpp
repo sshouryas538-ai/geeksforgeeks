e5a13b116b8667642239d9a6c98d02eeafafcea3
Shourya Sharma <sshouryas538@gmail.com>
2026-09-16 20:37:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindromic Substring/longest-palindromic-substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string getLongestPal(string &s) {
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++) {
+
+            // Odd length palindrome
+            int f = i;
+            int l = i;
+
+            while (f >= 0 && l < s.size() && s[f] == s[l]) {
+                f--;
+                l++;
+            }
+
+            // f and l have gone one step too far
+            f++;
+            l--;
+
+            if (l - f + 1 > ans.size()) {
+                ans = s.substr(f, l - f + 1);
+            }
+
+
+            // Even length palindrome
+            f = i;
+            l = i + 1;
+
+            while (f >= 0 && l < s.size() && s[f] == s[l]) {
+                f--;
+                l++;
+            }
+
+            f++;
+            l--;
+
+            if (l - f + 1 > ans.size()) {
+                ans = s.substr(f, l - f + 1);
+            }
+        }
+
+        return ans;
+    }
+};
